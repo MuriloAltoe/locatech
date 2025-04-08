366e1220f9210f961d82ff360f63bdf23333822b
MuriloAltoe <muriloaleme17@gmail.com>
2025-04-08 18:51:28 -0300

    Desenvolvimento de APIs RESTful com Spring - I

---
+++ src/main/java/com/locatech/locatech/controller/entities/Aluguel.java
@@ -0,0 +1,31 @@
+package com.locatech.locatech.controller.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Aluguel {
+    private Long id;
+    private Long pessoaId;
+    private Long veiculoId;
+    private String veiculoModelo;
+
+    private String pessoaCpf;
+    private String pessoaNome;
+
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+    private BigDecimal valorTotal;
+}
